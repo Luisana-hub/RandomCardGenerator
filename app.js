22f0cf3c95b4341a8e0a5e7c2e04f9737104fe45
Luisana Viamonte <luisanaviamonte@gmail.com>
2021-02-25 17:17:24 -0300

    Version final

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 function devuelveNumero(){
     let numero = ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A'];
     let positionNumero = (Math.floor(Math.random() * numero.length + 0));
-    if(numero[positionNumero]=='10'){
+
+    if(numero[positionNumero]=='10'){   // condicion para mejorar la posicion del numero 10
         document.getElementById("numero").style.left = "32%";
-       
     }
 
     document.getElementById("numero").innerHTML = numero[positionNumero];
-    
 }
 
 function devuelvePinta(){
     let pinta = ['♦','♥','♠','♣'];
     let positionPinta = (Math.floor(Math.random() * pinta.length + 0));
-    if(pinta[positionPinta]=='♦' || pinta[positionPinta]=='♥'){
+
+    if(pinta[positionPinta]=='♦' || pinta[positionPinta]=='♥'){   //Condicion para escoger el color de la pinta
         document.getElementById("pintaizquierda").style.color = "red";
         document.getElementById("pintaderecha").style.color = "red";
     } else{
@@ -22,7 +22,6 @@ function devuelvePinta(){
     }
     document.getElementById("pintaizquierda").innerHTML = pinta[positionPinta];
     document.getElementById("pintaderecha").innerHTML = pinta[positionPinta];
-
 }
 console.log(devuelveNumero());
 console.log(devuelvePinta());
